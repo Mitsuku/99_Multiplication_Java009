e994c25ac1ae4ac2ca0ea56bd3f9beadf0388d19
Mitsuku <kjfgkfdkc@gmail.com>
2019-11-07 11:27:38 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,15 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        for(int x = 1 ; x <=9 ;x++){
+            for(int y = 1;y<=9 ;y++){
+                System.out.print(x+"*"+y+"="+x*y+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
